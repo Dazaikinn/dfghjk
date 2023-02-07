e63f33c24bec79e94b016f10d829d532bf3071a0
Dazaikinn <122983338+Dazaikinn@users.noreply.github.com>
2023-02-07 17:39:16 +0500

    Add files via upload

---
+++ js/247.js
@@ -0,0 +1,24 @@
+// let elem = document.querySelector('#elem');
+// let elem1 = document.querySelector('#elem1');
+// elem1.addEventListener('click', func1);
+// function func1() {
+// 	console.log(elem.textContent);
+// }
+
+// let elem = document.querySelector('#elem');
+// let elem1 = document.querySelector('#elem1');
+// elem1.addEventListener('click', func1);
+// function func1() {
+// 	console.log(elem.textContent);
+//     elem.textContent = 'на улице темно!';
+// }
+
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// elem3.addEventListener('click', func1);
+// function func1()
+// 	console.log(Number(elem1.textContent) + Number(elem2.textContent));
+// 
+
+
